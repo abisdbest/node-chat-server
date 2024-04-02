4da7757f43f3f8301d2dd2f4e2881b553fc31a47
AG123-321 <123011074+AG123-321@users.noreply.github.com>
2024-04-02 16:02:31 +0000

    hmmm

--- server.js
+++ server.js
@@ -115,13 +115,19 @@ app.post('/deletemessage', (req, res) => {
   console.log("thingied 2")
   if (!username || !message || !password) {res.status(400).json({ error: "Not enough parameters" })}
   else if (password == "aaa"){
-    for (let i = 0; i < messages.length; i++) {
-      if (messages[i].username == username && messages[i].message == message) {
-          messages.splice(i, 1)
-          break
+    try {
+      for (let i = 0; i < messages.length; i++) {
+        if (messages[i].username == username && messages[i].message == message) {
+            messages.splice(i, 1)
+            break
+        }
       }
     }
-    res.status(200).json({ deleted: true })
+
+    catch (e) {
+      res.status(200).json({ deleted: false, error: e })  
+    }
+    res.status(200).json({ deleted: true, error: null })
   }
 });
 
